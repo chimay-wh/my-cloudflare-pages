b070f28e74f2177786d2c4e20569c84bcd0c51a0
Yasuhiro Katoh <76897405+chimay-wh@users.noreply.github.com>
2025-06-18 14:46:50 +0900

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,25 +1,13 @@
 export async function onRequest({ request, env, next }) {
-    console.log("USERNAME:", env.BASIC_USERNAME);
-    console.log("PASSWORD:", env.BASIC_PASSWORD);
+    console.log("DEBUG: Middleware is running.");
+    console.log("DEBUG: USERNAME =", env.BASIC_USERNAME);
+    console.log("DEBUG: PASSWORD =", env.BASIC_PASSWORD);
 
     if (!request.headers.has('Authorization')) {
         return new Response('You need to login.', {
             status: 401,
             headers: { 'WWW-Authenticate': 'Basic realm=\"Restricted Area\"' },
         });
-    }
-
-    const [scheme, encoded] = request.headers.get('Authorization').split(' ');
-    if (!encoded || scheme !== 'Basic') {
-        return new Response('Malformed authorization header.', { status: 400 });
-    }
-
-    const decoded = atob(encoded);
-    const [username, password] = decoded.split(':');
-
-    if (username !== env.BASIC_USERNAME || password !== env.BASIC_PASSWORD) {
-        console.log("認証失敗: 入力された username=", username, " password=", password);
-        return new Response('Invalid username or password.', { status: 401 });
     }
 
     return next();
