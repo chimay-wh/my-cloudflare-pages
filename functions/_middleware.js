f5d4be8966b972954555115e9f162a8956099096
Yasuhiro Katoh <76897405+chimay-wh@users.noreply.github.com>
2025-06-18 14:48:58 +0900

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -4,10 +4,25 @@ export async function onRequest({ request, env, next }) {
     console.log("DEBUG: PASSWORD =", env.BASIC_PASSWORD);
 
     if (!request.headers.has('Authorization')) {
+        console.log("DEBUG: Authorization header missing");
         return new Response('You need to login.', {
             status: 401,
-            headers: { 'WWW-Authenticate': 'Basic realm=\"Restricted Area\"' },
+            headers: { 'WWW-Authenticate': 'Basic realm="Restricted Area"' },
         });
+    }
+
+    const [scheme, encoded] = request.headers.get('Authorization').split(' ');
+    if (!encoded || scheme !== 'Basic') {
+        console.log("DEBUG: Malformed authorization header");
+        return new Response('Malformed authorization header.', { status: 400 });
+    }
+
+    const decoded = atob(encoded);
+    const [username, password] = decoded.split(':');
+
+    if (username !== env.BASIC_USERNAME || password !== env.BASIC_PASSWORD) {
+        console.log(`DEBUG: Invalid login attempt - username=${username}, password=${password}`);
+        return new Response('Invalid username or password.', { status: 401 });
     }
 
     return next();
