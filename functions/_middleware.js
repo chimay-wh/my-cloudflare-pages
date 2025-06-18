92a1f6e3cc6020a5b512fcf582cfa81f7435af5d
Yasuhiro Katoh <76897405+chimay-wh@users.noreply.github.com>
2025-06-18 13:12:56 +0900

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,8 +1,11 @@
 export async function onRequest({ request, env, next }) {
+    console.log("USERNAME:", env.BASIC_USERNAME);
+    console.log("PASSWORD:", env.BASIC_PASSWORD);
+
     if (!request.headers.has('Authorization')) {
         return new Response('You need to login.', {
             status: 401,
-            headers: { 'WWW-Authenticate': 'Basic realm="Restricted Area"' },
+            headers: { 'WWW-Authenticate': 'Basic realm=\"Restricted Area\"' },
         });
     }
 
@@ -15,6 +18,7 @@ export async function onRequest({ request, env, next }) {
     const [username, password] = decoded.split(':');
 
     if (username !== env.BASIC_USERNAME || password !== env.BASIC_PASSWORD) {
+        console.log("認証失敗: 入力された username=", username, " password=", password);
         return new Response('Invalid username or password.', { status: 401 });
     }
 
